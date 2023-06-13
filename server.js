ad9fafd0c6bce6788b2d18b5effbae4981ee269c
Nathan SUK <46647779+nathansuk@users.noreply.github.com>
2023-06-13 17:56:01 +0200

    smol

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 8080 });
+console.log('Serveur socket démarré')
 
 wss.on('connection', function connection(ws) {
   console.log('Connexion au serveur socket')
