e37fff56ade6887e2faf6d465e4b167478f6a421
Nathan SUK <46647779+nathansuk@users.noreply.github.com>
2023-06-21 14:36:05 +0200

    saving datas into database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const { WebSocketServer } = require('ws');
+const mongoose = require('mongoose')
+const StationData = require('./src/Database/StationData')
 
-const wss = new WebSocketServer({ port: 8080 });
+async function main() {
+    mongoose.connect('mongodb://127.0.0.1:27017/meteo')
+    console.log('Connexion à la base de données effectuée.')
+}
+
+main().catch(error => console.log(error))
+
+const wss = new WebSocketServer({ port: 8081 });
 const connections = []
 console.log('Serveur socket démarré')
 
@@ -14,18 +23,31 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data) {
     const jsonObject = JSON.parse(data)
+
     const transformedData = {
       "stationId": jsonObject["stationId"],
-      "dataDate": new Date(),
+      "dataDate": Date.now(),
       "datas": jsonObject["datas"]
     }
+
+
     console.log('Reçu: %s', JSON.stringify(transformedData));
 
-    connections.forEach(connection => {
-      connection.send(JSON.stringify(transformedData))
+    const stationDatas = new StationData({
+      stationId: transformedData["stationId"],
+      dataDate: transformedData["dataDate"],
+      datas: transformedData["datas"]
     })
-    console.log('EMISSION EFFECTUEE');
+
+    stationDatas.save().then(() => {
+      console.log("Data enregistrées")
+      connections.forEach(connection => {
+        connection.send(JSON.stringify(transformedData))
+      })
+      console.log('EMISSION EFFECTUEE');
     
+    })
+
   });
 
 });
